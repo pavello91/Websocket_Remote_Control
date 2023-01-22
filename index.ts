78d9809f65baafa6c9cf0aa2e7c14465f096f6a4
pavello91 <pavlentianno27@gmail.com>
2023-01-22 20:31:50 +0300

    navigate mouse

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 import { httpServer } from "./src/http_server/index";
 import { WebSocketServer, createWebSocketStream } from 'ws';
+import navigationController from './src/navigation';
 import { mouse } from "@nut-tree/nut-js";
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const commands = {
+    navigation: [
+      'mouse_up',
+      'mouse_left',
+      'mouse_down',
+      'mouse_right',
+      'mouse_position'
+    ]}
 
 const wss = new WebSocketServer({port: 8080});
 
@@ -14,8 +24,16 @@ wss.on('connection', (ws, request) => {
     const wsStream = createWebSocketStream(ws, {encoding: 'utf-8', decodeStrings: false})
     wsStream.on('data', async (chunk) => {
       const [command, ...args] = chunk.toString().split(' ')
+      if (commands.navigation.includes(command)) {
+        const res = navigationController(command, args)
+        ws.send(res)
+      }
      
     })
 
-}
-)
+})
+
+process.on('SIGINT', () => {
+    console.log('closed wss');
+    wss.close()
+  })
